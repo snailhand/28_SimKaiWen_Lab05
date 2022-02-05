5a79452adc4915cdfe8ff4dd171e9795405d6508
simkaiwen <kaiphonegt@gmail.com>
2022-02-05 22:09:14 +0800

    Created SceneHandler

---
+++ Assets/Script/SceneHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoseScene()
+    {
+        SceneManager.LoadScene("LoseScene", LoadSceneMode.Additive);
+    }
+
+    public void WinScene()
+    {
+        SceneManager.LoadScene("WinScene", LoadSceneMode.Additive);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
